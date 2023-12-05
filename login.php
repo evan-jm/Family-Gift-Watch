886d3fa623b0269f474b80867e2aaf9b7a4d952c
Evan Mitchell <mitch172@go.stockton.edu>
2023-12-05 10:33:25 -0500

    Modified login

--- login.php
+++ login.php
@@ -5,17 +5,10 @@
     $dbname = "myDB";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
-<<<<<<< HEAD
-
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-=======
     
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     $sql = "INSERT INTO USERS (FirstName, LastName, Username, Password)
         VALUES ($firstName, $lastName, $username, $password)";
->>>>>>> refs/remotes/origin/main
 ?>
