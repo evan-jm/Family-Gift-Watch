f99311c55e9318ffab33b8550682b19db54f25b5
Evan Mitchell <114937545+mitch172@users.noreply.github.com>
2024-01-01 02:40:46 -0500

    Update register.php

--- authentication/register.php
+++ authentication/register.php
@@ -7,6 +7,11 @@
 		$password = $_POST['password'];
 		$sql = "INSERT INTO USERS (FirstName, LastName, Username, Password)
 		VALUES ($firstName, $lastName, $userName, $password)";
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 		$conn->close();
 	}
 ?>
